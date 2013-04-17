85b21633eaa38ae489fd1740de090cb687a6cb55
Vitaly Driedfruit <driedfruit@mindloop.net>
2013-04-17 07:18:15 +0400

    Include needed SDL header from the .h

--- savepng.h
+++ savepng.h
@@ -4,7 +4,7 @@
  * This code is free software, available under zlib/libpng license.
  * http://www.libpng.org/pub/png/src/libpng-LICENSE.txt
  */
-
+#include <SDL_video.h>
 /*
  * Save an SDL_Surface as a PNG file.
  *
