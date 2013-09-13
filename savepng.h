953c8ed0246a6e7ad7225209d3959642ea06ecd6
Vitaly Driedfruit <driedfruit@mindloop.net>
2013-09-13 12:01:29 +0400

    Add guard ifdef to .h file.

--- savepng.h
+++ savepng.h
@@ -1,3 +1,5 @@
+#ifndef _SDL_SAVEPNG
+#define _SDL_SAVEPNG
 /*
  * SDL_SavePNG -- libpng-based SDL_Surface writer.
  *
@@ -30,3 +32,5 @@ extern int SDL_SavePNG_RW(SDL_Surface *surface, SDL_RWops *rw, int freedst);
  * Return new SDL_Surface with a format suitable for PNG output.
  */
 extern SDL_Surface *SDL_PNGFormatAlpha(SDL_Surface *src);
+
+#endif
